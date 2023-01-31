417d2813414b1110f7a03369233e07aa72658a5d
khrassi zlateff <krasimir.zlateff@gmail.com>
2023-01-31 10:20:19 +0200

    lib: angular material

--- apps/debt-collector/src/app/app.module.ts
+++ apps/debt-collector/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Added
+import { MaterialModule } from '@eos/material';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
